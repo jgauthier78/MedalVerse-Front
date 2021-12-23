4fc5c0149ee2fc5a044118525f67fc67ff67789c
BenoistP <75934369+BenoistP@users.noreply.github.com>
2021-12-23 23:06:08 +0100

    DID

---
+++ client/src/utils/did.js
@@ -0,0 +1,96 @@
+// Ceramic / IDX / 3ID/Connect
+import CeramicClient from '@ceramicnetwork/http-client'
+import { IDX } from '@ceramicstudio/idx'
+import { EthereumAuthProvider, ThreeIdConnect } from '@3id/connect'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+import { DID } from 'dids'
+
+import { CERAMIC_ENDPOINT, BASIC_PROFILE_ALIAS } from './did_CONSTS'
+
+
+const DID_init = async ( web3, /*window.ethereum*/ provider ) =>
+{
+    console.log("DID_init")
+    return [ web3, web3.eth.net.getId(), web3.eth.accounts[0], provider ];
+} // init
+
+const { WEB3, networkId, accountAddress, PROVIDER } = DID_init ;
+
+const DID_showConf = () =>
+{
+    console.log("DID_showConf : networkId = " + networkId + " accountAddress = " + accountAddress )
+}
+
+const DID_readProfile = async ( /*address, web3 */ /*, endpoint*/ ) =>
+{
+    //const [address] = await connect()
+    const ceramic = new CeramicClient(/*endpoint*/CERAMIC_ENDPOINT)
+    const idx = new IDX({ ceramic })
+
+    try
+    {
+      const data = await idx.get
+      (
+        // 'basicProfile', // basic profile index alias
+        BASIC_PROFILE_ALIAS, // index alias 
+        //`${address}@eip155:1`
+        didAddress()
+      )
+      console.log('data: ', data)
+    //   if (data.name) setName(data.name)
+    //   if (data.avatar) setImage(data.avatar)
+      return data;
+    } catch (error)
+    {
+      console.log('error: ', error)
+//      setLoaded(true)
+        return undefined;
+    }
+} // readProfile
+
+const DID_updateProfile = async ( /*address, web3, *//* endpoint*/ data ) =>
+{
+//    const [address] = await connect()
+    const ceramic = new CeramicClient(/*endpoint*/ CERAMIC_ENDPOINT )
+    const threeIdConnect = new ThreeIdConnect()
+    const provider = new EthereumAuthProvider( 
+        PROVIDER, // Provider
+         
+         accountAddress // address
+         )
+
+    await threeIdConnect.connect(provider)
+
+    const did = new DID({
+      provider: threeIdConnect.getDidProvider(),
+      resolver: {
+        ...ThreeIdResolver.getResolver(ceramic)
+      }
+    })
+
+    ceramic.setDID(did)
+    await ceramic.did.authenticate()
+
+    const idx = new IDX({ ceramic })
+
+    await idx.set(
+        //'basicProfile',
+        BASIC_PROFILE_ALIAS, // index alias 
+        data
+    //  {
+      
+    //   name,
+    //   avatar: image
+    // }
+    )
+
+    console.log("Profile updated!")
+} // updateProfile
+
+const didAddress = () =>
+{
+    return `${accountAddress}@eip155:${networkId}`
+}
+
+export { DID_init, DID_readProfile, DID_updateProfile, DID_showConf };
+// export default readProfile;
---
+++ client/src/utils/did_CONSTS.js
@@ -0,0 +1,13 @@
+/* DID Ceramic Consts */
+
+// Ceramic
+// https://developers.ceramic.network/run/nodes/community-nodes/
+const Ceramic_Clay_testnet_endpoint = "https://ceramic-clay.3boxlabs.com"
+
+const CERAMIC_ENDPOINT = Ceramic_Clay_testnet_endpoint;
+
+// 80001
+
+const BASIC_PROFILE_ALIAS = 'basicProfile';
+
+export { CERAMIC_ENDPOINT, BASIC_PROFILE_ALIAS }
