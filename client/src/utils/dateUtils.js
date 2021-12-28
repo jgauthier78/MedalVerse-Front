eae73db3d34844c21e80d36003588c9d5a1d0459
BenoistP <75934369+BenoistP@users.noreply.github.com>
2021-12-28 22:34:57 +0100

    Update dateUtils.js

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -1,6 +1,12 @@
 /* Traduction */
 import { t } from "i18next";
 
+const formatOptionsWithTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit", timeZoneName: "short"}
+const formatOptionsWithoutTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit"}
+
+// const formatOptionsWithTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit", timeZoneName: "short"}
+// const formatOptionsWithoutTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit"}
+
 const format_TimeStampToStartDate = (timestampinSeconds) =>{
     return format_TimeStampToDate(timestampinSeconds, false)
 }
@@ -10,13 +16,11 @@ const format_TimeStampToEndDate = (timestampinSeconds) =>{
 
 const format_TimeStampToDate = (timestampinSeconds, withTZ) =>{
     const timestampInMs = timestampinSeconds * 1000
-    const formatOptionsWithTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit", timeZoneName: "short"}
-    const formatOptionsWithoutTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit"}
     const formatOptions = ( withTZ ? formatOptionsWithTZ : formatOptionsWithoutTZ )
     // console.log("format_TimeStampToDate: timestampInMs="+timestampInMs)
-    let jsDate = new Date(timestampInMs);
     // console.log( "jsDate toUTCString = " + jsDate.toUTCString() )
-    let sDate = jsDate.toLocaleDateString( t("Formats.date"), formatOptions )
+    let jsDate = new Date(timestampInMs);
+    let sDate = new Intl.DateTimeFormat( t("Formats.date") ).format( jsDate ) + " " + new Intl.DateTimeFormat( t("Formats.date"), formatOptions ).format( jsDate )
     return sDate
 }
 
