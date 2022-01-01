bd69e70009c9ab890bf4799edb182a25be6baa3b
Bloodyyyr <jdss.51100@gmail.com>
2022-01-01 12:07:33 +0100

    Update commentaire

--- test/NFTArtist-test.js
+++ test/NFTArtist-test.js
@@ -14,15 +14,17 @@ contract('NFTArtist', function (accounts) {
         this.NFTArtistInstance = await NFTArtist.new({from: owner});
 
     })
-
+    // Mint --------------
     it("mint D'un NFT Artiste et vérification de ça structure", async function () {
         await this.NFTArtistInstance.mintNFTArtist(name, Uri, {from: user});
-
         let balance = new BN(await this.NFTArtistInstance.balanceOf(user)); 
+
+        //Check 
         expect(balance).to.be.bignumber.equal(number)
 
         let NFT = await this.NFTArtistInstance.NFTByOwner(user, number);
 
+        //Check
         expect(NFT.name).to.equal(name);
         expect(new BN(NFT.tokenId)).to.be.bignumber.equal(number);
         expect(NFT.creator).to.equal(user);
