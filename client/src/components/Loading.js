f076bb71cbc45a37e83c997e67c00a782ca2a20a
BenoistP <75934369+BenoistP@users.noreply.github.com>
2022-01-10 22:40:27 +0100

    Update Loading.js
    taille du bandeau

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -9,7 +9,7 @@ const Loading = ( /* {  } */ ) =>
    return (
     <Container fluid expand="lg">
       <Row style={{backgroundColor: 'rgba(53, 112, 238, 0.829)'}}>
-        <Col xs lg="12" style={{ height: '90px'}}>
+        <Col xs lg="12" style={{ height: '70px'}}>
         Loading ... <Clock style={{verticalAlign: '-10%'}}/>
         </Col>
       </Row>
