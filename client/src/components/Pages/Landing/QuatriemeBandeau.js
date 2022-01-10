794021f88753a9195e35a164afb00343f51ef6b9
BenoistP <75934369+BenoistP@users.noreply.github.com>
2022-01-10 18:11:19 +0100

    Traduction QuatrièmeBandeau

--- client/src/components/Pages/Landing/QuatriemeBandeau.js
+++ client/src/components/Pages/Landing/QuatriemeBandeau.js
@@ -1,17 +1,25 @@
-
+// React - Bootstrap
 import { Container, Row, Col } from "react-bootstrap";
 
+// Icons
 import { Facebook, Google, Instagram, Linkedin, Pinterest, Twitter } from 'react-bootstrap-icons';
+
+// Translation
+import { useTranslation } from 'react-i18next';
+
+// Components
 import NavigateButton from "../../UIElements/NavigateButton";
 
-let QuatriemeBandeau = () => {
+const QuatriemeBandeau = () => {
+    const { t } = useTranslation();
+    
     return (
         <section id="communeaute" className="section Blue-Bkg">
             <Container>
                 <Row className="row-grid align-items-center mt-6">
                     <div md="4">
-                        <h1 className="display-1 text-white d-flex justify-content-center">Communeauté</h1>
-                        <h4 className="text-gray d-flex justify-content-center">{"Organisateurs, Sportifs, Fans: Rejoignez-nous !"}</h4>
+                        <h1 className="display-1 text-white d-flex justify-content-center">{t("QuatriemeBandeau.title")}</h1>
+                        <h4 className="text-gray d-flex justify-content-center">{t("QuatriemeBandeau.text")}</h4>
                     </div>
                     <div className="d-flex justify-content-center mt-8">
 
