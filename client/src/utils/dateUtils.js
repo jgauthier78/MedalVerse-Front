ca48eef65d99d6533fc5ba74e12b7e861f19114f
BenoistP <75934369+BenoistP@users.noreply.github.com>
2022-01-13 10:17:40 +0100

    Modification des options de formatage des dates

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -2,21 +2,23 @@
 import { t } from "i18next";
 
 const formatOptionsWithTZ = { hour: "2-digit", minute: "2-digit", second: "2-digit", timeZoneName: "short" }
+const formatOptionsWithTZWithoutSeconds = { hour: "2-digit", minute: "2-digit", timeZoneName: "short" }
 const formatOptionsWithoutTZ = { hour: "2-digit", minute: "2-digit", second: "2-digit" }
+const formatOptionsWithoutTZWithoutSeconds = { hour: "2-digit", minute: "2-digit" }
 
 // const formatOptionsWithTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit", timeZoneName: "short"}
 // const formatOptionsWithoutTZ = {hour: "2-digit", minute: "2-digit", second: "2-digit"}
 
-const format_TimeStampToStartDate = (timestampinSeconds) => {
-    return format_TimeStampToDate(timestampinSeconds, false)
+const format_TimeStampToStartDate = (timestampinSeconds, withTZ=false, withSeconds=false) => {
+    return format_TimeStampToDate(timestampinSeconds, withTZ, withSeconds )
 }
-const format_TimeStampToEndDate = (timestampinSeconds) => {
-    return format_TimeStampToDate(timestampinSeconds, true)
+const format_TimeStampToEndDate = (timestampinSeconds, withTZ=false, withSeconds=false) => {
+    return format_TimeStampToDate(timestampinSeconds, withTZ, withSeconds )
 }
 
-const format_TimeStampToDate = (timestampinSeconds, withTZ) => {
+const format_TimeStampToDate = (timestampinSeconds, withTZ=false, withSeconds=false) => {
     const timestampInMs = timestampinSeconds * 1000
-    const formatOptions = (withTZ ? formatOptionsWithTZ : formatOptionsWithoutTZ)
+    const formatOptions = (withTZ ? (withSeconds ? formatOptionsWithTZ : formatOptionsWithTZWithoutSeconds) : (withSeconds ? formatOptionsWithoutTZ : formatOptionsWithoutTZWithoutSeconds))
     // console.log("format_TimeStampToDate: timestampInMs="+timestampInMs)
     // console.log( "jsDate toUTCString = " + jsDate.toUTCString() )
     let jsDate = new Date(timestampInMs);
@@ -26,7 +28,6 @@ const format_TimeStampToDate = (timestampinSeconds, withTZ) => {
 
 const format_Date = (timestampinSeconds) => {
     const timestampInMs = timestampinSeconds * 1000
-
     // console.log("format_TimeStampToDate: timestampInMs="+timestampInMs)
     // console.log( "jsDate toUTCString = " + jsDate.toUTCString() )
     let jsDate = new Date(timestampInMs);
@@ -34,12 +35,12 @@ const format_Date = (timestampinSeconds) => {
     return sDate
 }
 
-const format_TimeMsToDate = (timestampInMs, withTZ) => {
-    const formatOptions = (withTZ ? formatOptionsWithTZ : formatOptionsWithoutTZ)
+const format_TimeMsToDate = (timestampInMs, withTZ=false, withSeconds=false) => {
+    const formatOptions = (withTZ ? (withSeconds ? formatOptionsWithTZ : formatOptionsWithTZWithoutSeconds) : (withSeconds ? formatOptionsWithoutTZ : formatOptionsWithoutTZWithoutSeconds))
     let jsDate = new Date(timestampInMs);
     let sDate = new Intl.DateTimeFormat(t("Formats.date")).format(jsDate) + " " + new Intl.DateTimeFormat(t("Formats.date"), formatOptions).format(jsDate)
     return sDate
 }
 
-
+// Exports
 export { format_TimeStampToStartDate, format_TimeStampToEndDate, format_Date, format_TimeMsToDate };
